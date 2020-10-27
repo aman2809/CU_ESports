8cfc5a2e0831ecbede7cd933fbb665c07b3aded7
aman2809 <amansaxena2809@gmail.com>
2020-10-27 18:27:22 +0530

    Profile Page With Back To Home Button

--- app/src/main/java/com/example/finalapp/Log_in.java
+++ app/src/main/java/com/example/finalapp/Log_in.java
@@ -17,6 +17,7 @@ public class Log_in extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         Button sign_up = findViewById(R.id.signup_button);
+        Button profile_button = findViewById(R.id.profile_button);
         //Sign Up Button Action
         sign_up.setOnClickListener(new View.OnClickListener()
         {
@@ -28,6 +29,19 @@ public class Log_in extends AppCompatActivity {
             public void Sign_in()
             {
                 Intent intent = new Intent(Log_in.this, Sign_in.class);
+                startActivity(intent);
+            }
+        });
+        profile_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                profile_button();
+            }
+            public void profile_button()
+            {
+                Intent intent = new Intent(Log_in.this, Profile.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/finalapp/Profile.java
+++ app/src/main/java/com/example/finalapp/Profile.java
@@ -2,7 +2,10 @@ package com.example.finalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Button back_to_home = findViewById(R.id.back_to_home_button);
+        //Back To Home Button
+        back_to_home.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                back_to_home();
+            }
+            public void back_to_home()
+            {
+                Intent intent = new Intent(Profile.this, Log_in.class);
+                startActivity(intent);
+            }
+        });
     }
 }
